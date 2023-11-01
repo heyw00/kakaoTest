2f936890c8276999d35762cf162035152545fbb5
heyw00 <heywoo12@gmail.com>
2023-11-01 18:28:15 +0900

    test

--- kakao.js
+++ kakao.js
@@ -1,6 +1,6 @@
 function scrapKakao() {
-    const title = '대한민국 1등 옷장 정리 서비스';
-    const description = '해당 프로모션 링크로 서비스 신청 시 즉시 할인 혜택을 받을 수 있습니다!';
+    const title = '추천인 프로모션';
+    const description = '해당 링크로 서비스 신청 시 즉시 할인 혜택을 받을 수 있습니다!';
     const pathName = 'form/reservation?promotionCode=123'; 
 
     Kakao.Share.sendCustom({
