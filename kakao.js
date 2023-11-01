61ef21766936275f3a1aba0d7d2ffa284381efcf
heyw00 <heywoo12@gmail.com>
2023-11-01 17:24:32 +0900

    url 추가

--- kakao.js
+++ kakao.js
@@ -1,8 +1,8 @@
 function scrapKakao() {
     const title = '대한민국 1등 옷장 정리 서비스';
     const description = '해당 프로모션 링크로 서비스 신청 시 즉시 할인 혜택을 받을 수 있습니다!';
-    const originUrl = '';
-    const pathName = ''; 
+    const originUrl = 'https://heyw00.github.io/';
+    const pathName = 'kakaoTest/'; 
 
     Kakao.Link.sendScrap({
       requestUrl: originUrl + pathName,
