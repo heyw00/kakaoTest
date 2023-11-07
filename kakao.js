80693329fc653550679a9f96565ff15f2a7c3fa7
heyw00 <120547603+heyw00@users.noreply.github.com>
2023-11-07 13:58:04 +0900

    Update kakao.js
    test

--- kakao.js
+++ kakao.js
@@ -1,7 +1,7 @@
 function scrapKakao() {
     const title = '대한민국 1등 옷장 정리 서비스';
     const description = '해당 프로모션 링크로 서비스 신청 시 즉시 할인 혜택을 받을 수 있습니다!';
-    const pathName = 'form/reservation?promotionCode=123'; 
+    const pathName = 'Me-9506a23f35f8457c92bf6dc1f933234d?pvs=4#84abccaba4b044cb8409767e1daa44dd'; 
 
     Kakao.Share.sendCustom({
       templateId: 100221,
