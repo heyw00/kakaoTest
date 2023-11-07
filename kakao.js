12905517e701affdccf2feae552281f0771cb4e9
heyw00 <120547603+heyw00@users.noreply.github.com>
2023-11-07 15:23:04 +0900

    Update kakao.js

--- kakao.js
+++ kakao.js
@@ -1,4 +1,4 @@
-function scrapKakao() {
+function shareKakao() {
     const title = '추천인 프로모션';
     const description = '해당 링크로 서비스 신청 시 즉시 할인 혜택을 받을 수 있습니다!';
     const pathName = 'form/reservation?promotionCode=123'; 
